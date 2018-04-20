8c71239b1d7ca6f2c8bd8cdfc8fe4cd797a546c5
nicholasspiroff <nicholasspiroff@gmail.com>
2018-04-20 14:11:34 -0500

    Added MR_DefaultHashPartition

---
+++ linux/mapreduce.c
@@ -0,0 +1,9 @@
+#include "mapreduce.h"
+
+unsigned long MR_DefaultHashPartition(char *key, int num_partitions) {
+    unsigned long hash = 5381;
+    int c;
+    while ((c = *key++) != '\0')
+        hash = hash * 33 + c;
+    return hash % num_partitions;
+}
